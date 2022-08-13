3bc91cd767a9c8f7188b1c0a9ccb47195a7e92d1
WilliamMcRoberts <wamcroberts2@gmail.com>
2022-08-12 21:06:01 -0400

    refactor

--- BetBookData/Services/TimerService.cs
+++ BetBookData/Services/TimerService.cs
@@ -1,7 +1,6 @@
 ﻿
 using BetBookData.Interfaces;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
 namespace BetBookData.Services;
 
@@ -10,8 +9,9 @@ namespace BetBookData.Services;
 public class TimerService : BackgroundService
 {
     private readonly IGameService _gameService;
-    private readonly PeriodicTimer _timerForPointSpreadUpdate = new(TimeSpan.FromHours(4));
-    public TimerService(IGameService gameService, ILogger<TimerService> logger)
+    private readonly PeriodicTimer _timer = new(TimeSpan.FromHours(1));
+
+    public TimerService(IGameService gameService)
     {
         _gameService = gameService;
     }
@@ -19,7 +19,7 @@ public class TimerService : BackgroundService
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
         int index = 1;
-        while (await _timerForPointSpreadUpdate.WaitForNextTickAsync(stoppingToken)
+        while (await _timer.WaitForNextTickAsync(stoppingToken)
                     && !stoppingToken.IsCancellationRequested)
         {
             Console.WriteLine($"Timer trigger count: {index}");
