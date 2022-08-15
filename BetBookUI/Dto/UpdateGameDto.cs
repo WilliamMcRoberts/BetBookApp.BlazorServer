bbf585abd62b4c4a886b7e79a90a1fa6ef18c261
WilliamMcRoberts <wamcroberts2@gmail.com>
2022-08-14 21:24:17 -0400

    refactor

--- BetBookUI/Dto/UpdateGameDto.cs
+++ BetBookUI/Dto/UpdateGameDto.cs
@@ -17,24 +17,15 @@ public class UpdateGameDto
     [Range(1, 32, ErrorMessage = "Away Team Id must be in the range of 1 - 32")]
     public int AwayTeamId { get; set; }
 
-    // Id of team that is declared the favorite
-    [Required]
-    [Range(1, 32, ErrorMessage = "Favorite Team Id must be in the range of 1 - 32")]
-    public int FavoriteId { get; set; }
-
-    // Id of team that is declared the underdog
-    [Required]
-    [Range(1, 32, ErrorMessage = "Underdog Team Id must be in the range of 1 - 32")]
-    public int UnderdogId { get; set; }
 
     // Stadium the game is being played in
     [Required]
     [MinLength(7, ErrorMessage = "Stadium is required and must be more than 7 characters")]
-    public string Stadium { get; set; }
+    public string Stadium { get; set; } = string.Empty;
 
     // The point spread of the game
     [Required]
-    public string PointSpread { get; set; }
+    public string PointSpread { get; set; } = string.Empty;
 
     // Date of the game
     [Required]
@@ -44,17 +35,17 @@ public class UpdateGameDto
 
     public double AwayTeamFinalScore { get; set; }
 
-    public int GameWinnerId { get; set; }
+    public int? GameWinnerId { get; set; }
 
     [Required]
     [Range(0, 17, ErrorMessage = "Week number must be from 0 to 17")]
     public int Week { get; set; }
 
     [Required]
-    public string Season { get; set; }
+    public string Season { get; set; } = string.Empty;
 
     [Required]
-    public string GameStatus { get; set; }
+    public string GameStatus { get; set; } = string.Empty;
 
     [Required]
     public int ScoreId { get; set; }
