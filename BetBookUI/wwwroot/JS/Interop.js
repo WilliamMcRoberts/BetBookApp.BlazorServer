5b409ea8ebc89ffa99a38f5cb543b4723693a37d
WilliamMcRoberts <wamcroberts2@gmail.com>
2022-08-21 18:57:15 -0400

    JS Interop refactor

--- BetBookUI/wwwroot/JS/Interop.js
+++ BetBookUI/wwwroot/JS/Interop.js
@@ -4,11 +4,12 @@
 var prevScrollPos = window.pageYOffset;
 window.onscroll = function () {
     var currentScrollPos = window.pageYOffset;
-    if (prevScrollPos > currentScrollPos) {
-        document.getElementById("header").style.top = "0";
-    } else {
+    if (prevScrollPos <= currentScrollPos) {
         document.getElementById("header").style.top = "-100px";
+        prevScrollPos = currentScrollPos;
+        return;
     }
+    document.getElementById("header").style.top = "0";
     prevScrollPos = currentScrollPos;
 }
 
